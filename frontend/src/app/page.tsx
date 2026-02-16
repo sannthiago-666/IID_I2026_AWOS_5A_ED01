02527c6b6dc59fdc620a0f7ea970f4ceaafdd5a4
sanntiago.rvm <rvso240490@upemor.edu.mx>
2026-02-15 21:23:50 -0600

    amo a mi novia

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -2,6 +2,11 @@ import { Button } from "../components/ui/button";
 
 export default function Home() {
   return (
-    <Button variant="destructive">Boton</Button>
+    
+    <main>
+      <h1 className="text-3xl font-bold underline">Amo a mi novia</h1>
+      <Button variant="destructive">Boton</Button>
+    </main>
+    
   );
 }
